7ebb5617270d84336f797eac119df7c5c4eae6b0
allyavorsky <allyavorsky@proton.me>
2025-09-02 13:34:06 +0300

    HW39-1: add stateless Greeting component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greeting = ({ name }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Раді вас бачити на нашому сайті.</p>
+    </div>
+  );
+};
+
+export default Greeting;
